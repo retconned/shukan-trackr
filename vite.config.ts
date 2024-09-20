bbdc43bb33b53df4de30c705670d4fa872d3dda8
retconned <retcnned@gmail.com>
2024-09-20 10:11:30 +0100

    add pwa icons

--- vite.config.ts
+++ vite.config.ts
@@ -18,15 +18,30 @@ export default defineConfig({
         name: "Habit Tracker!",
         short_name: "HabitTracker",
         description: "Track your habits and maintain streaks!",
-        theme_color: "#ffffff",
+        theme_color: "#191615",
         icons: [
           {
-            src: "icon-192x192.png",
+            src: "/icons/16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "/icons/32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "/icons/180.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "/icons/192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "icon-512x512.png",
+            src: "/icons/512.png",
             sizes: "512x512",
             type: "image/png",
           },
